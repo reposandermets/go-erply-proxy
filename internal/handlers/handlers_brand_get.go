d9124f135b44ec6a304cbed35980f38588ce88e3
Sander Mets <sander.mets@builderhead.io>
2023-06-11 16:21:09 +0300

    Add caching for Get

--- internal/handlers/handlers_brand_get.go
+++ internal/handlers/handlers_brand_get.go
@@ -2,57 +2,43 @@ package handlers
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/erply/api-go-wrapper/pkg/api"
-	redis_utils "github.com/reposandermets/go-erply-proxy/internal/redis_utils"
+	"github.com/erply/api-go-wrapper/pkg/api/products"
+	"github.com/reposandermets/go-erply-proxy/internal/redis_utils"
 )
 
-type BrandResponse struct {
-	Added     int32  `json:"added,omitempty"`
-	Addedby   string `json:"addedby,omitempty"`
-	Changed   int32  `json:"changed,omitempty"`
-	Changedby string `json:"changedby,omitempty"`
-	Id        int32  `json:"id,omitempty"`
-	Name      string `json:"name,omitempty"`
-}
-
-func GetBrandsFromErplyAPI() {
-
-	sessionKey := "c2bb1db09a2ce295c67ae0c6b5ffee9c3a0327a319ba"
-	clientCode := "104791"
+func GetBrandsFromErplyAPI(sessionKey string, clientCode string) ([]products.ProductBrand, error) {
 
 	cli, err := api.NewClient(sessionKey, clientCode, nil)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	//configure the client to send the data payload in the request body instead of the query parameters.
-	//Using the request body eliminates the query size limitations imposed by the maximum URL length
 	cli.SendParametersInRequestBody()
 
-	//init context to control the request flow
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
 	brands, err := cli.ProductManager.GetBrands(ctx, nil)
-	// print the result
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	// println(brands)
 
-	fmt.Printf("Brand: %+v\n", brands)
-
+	return brands, nil
 }
 
 func V1BrandGet(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 
+	// TODO needs middleware to check if the user is authorized
 	split := strings.Split(r.Header.Get("Authorization"), ":")
 	if len(split) != 2 {
 		w.WriteHeader(http.StatusUnauthorized)
@@ -60,28 +46,53 @@ func V1BrandGet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	key1, key2 := redis_utils.GenerateUniqueKey(r)
-	data, _ := redis_utils.GetFromCache(r.Context(), key1+"|"+key2)
+	categoryKey, urlParamsKey := redis_utils.GenerateUniqueKey(r)
+	data, _ := redis_utils.GetFromCache(r.Context(), categoryKey+"|"+urlParamsKey)
 
 	if data == "" {
 		log.Println("Cache miss")
-		// Cache miss, get data from the database
-		// clientCode := split[0]
-		// sessionKey := split[1]
-		// data = GetBrandFromErplyAPI(r)
-
-		data = `{"id":1,"name":"Brand 1","description":{"en":"Brand 1 description"}}`
-		// Save data to cache
-		err := redis_utils.SaveToCache(r.Context(), key1, key2, data)
+		sessionKey := split[0]
+		clientCode := split[1]
+		brands, err := GetBrandsFromErplyAPI(sessionKey, clientCode)
 		if err != nil {
-			// Error occurred while saving to Redis
-			log.Printf("Error saving to cache: %v\n", err)
+			fmt.Println("Error:", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
 		}
-	} else {
-		// Cache hit, return data
-		log.Println("Cache hit")
+
+		jsonData, err := json.Marshal(brands)
+		if err != nil {
+			fmt.Println("Error:", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Println(string(jsonData))
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonData)
+
+		// Create a WaitGroup to synchronize goroutines
+		var wg sync.WaitGroup
+		wg.Add(1)
+
+		// Save data to cache asynchronously
+		go func() {
+			defer wg.Done()
+			err := redis_utils.SaveToCache(r.Context(), categoryKey, urlParamsKey, string(jsonData))
+			if err != nil {
+				// Handle error while saving to cache
+				log.Printf("Error saving to cache: %v\n", err)
+			}
+		}()
+
+		// Wait for the cache saving goroutine to complete
+		wg.Wait()
+		return
 	}
-	fmt.Printf("reqLookupKey: %s\n", data)
+
+	// Cache hit, return data
+	log.Println("Cache hit")
+
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(data))
 }
